966477f19b46df311452c28bd7089c24f1e0ea50
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-02-20 10:35:33 -0500

    initial compiler

---
+++ src/target/debug/deps/src-82aaf0b0854634e7.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\OneDrive\Desktop\NetBeans\repositorio\src\target\debug\deps\src-82aaf0b0854634e7.d: src\main.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\NetBeans\repositorio\src\target\debug\deps\libsrc-82aaf0b0854634e7.rmeta: src\main.rs
+
+src\main.rs:
---
+++ src/target/debug/deps/src-8b855eac29e192ad.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\OneDrive\Desktop\NetBeans\repositorio\src\target\debug\deps\src-8b855eac29e192ad.d: src\main.rs
+
+C:\Users\LENOVO\OneDrive\Desktop\NetBeans\repositorio\src\target\debug\deps\libsrc-8b855eac29e192ad.rmeta: src\main.rs
+
+src\main.rs:
