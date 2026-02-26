d85ef5aa620e482f640322cb662484d2ac4c1e1c
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-02-26 10:44:29 -0500

    Rust/Java

--- src/bin/src/bin/Bin.java
+++ src/bin/src/bin/Bin.java
@@ -1,6 +1,20 @@
 package bin;
+
+import java.util.Scanner;
+
 public class Bin {
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre[] = new String[8];
+        Scanner teclado = new Scanner (System.in);
+        
+        for (int i = 0; i < nombre.length; i++){
+            System.out.println("Ingrese el nombre que quiere guardar");
+            nombre[i] = teclado.nextLine();
+        }
+        for(int i = 0; i < nombre.length; i++){
+            System.out.print("El nombre: " + nombre[i] + " ");
+            System.out.println("Esta guardado en el indice " + i);
+        }
     }
 }
