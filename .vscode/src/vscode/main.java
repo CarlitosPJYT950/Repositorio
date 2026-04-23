95167b852ff69e4d4dfe7fa15b4d6de1223e13f3
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-04-23 14:41:28 -0500

    staged

--- .vscode/src/vscode/main.java
+++ .vscode/src/vscode/main.java
@@ -2,7 +2,7 @@ package vscode;
 
 import igu.agenda;
 
-public class vscode {
+public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         agenda electronica = new agenda();
